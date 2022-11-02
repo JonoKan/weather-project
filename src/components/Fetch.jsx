6938b1c9acd484408e2ed5dbdf21dbdcc9c9d73a
JonoKan2001 <79693698+JonoKan2001@users.noreply.github.com>
2022-11-02 18:11:01 +1300

    Fixed dimensions to fit on phone screen

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,4 +1,4 @@
-// Function to fetch data from pokeAPI urls
+// Function to fetch data from webAPI urls
 export async function fetchData(url) {
   const response = await fetch(url);
   var data = await response.json();
